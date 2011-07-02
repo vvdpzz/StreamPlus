d4704bd1ee196e6babaf266443060fdae3edfc59
VvDPzZ <vvdpzz@gmail.com>
2011-07-02 14:58:31 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Streamplus::Application.config.secret_token = '81179f9e48e916744e37934d5a2ebd32154453806c939676db699910f59b694723ad9f88959da7bf344e44e8f92989dd3d2963a580752cfe6d2683728734c589'
